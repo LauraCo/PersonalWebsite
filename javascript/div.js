39af4c22bd6c6c518ec5451b6ac629ddf45740cc
LauraCo <lac32@aber.ac.uk>
2014-03-03 12:40:43 +0000

    First commit of personal website

---
+++ javascript/div.js
@@ -0,0 +1,10 @@
+function showonlyone(thechosenone) {
+     $('.newboxes').each(function(index) {
+          if ($(this).attr("id") == thechosenone) {
+               $(this).show(200);
+          }
+          else {
+               $(this).hide(600);
+          }
+     });
+}
